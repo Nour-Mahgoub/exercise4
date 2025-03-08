68d3d5a4a9af6df007276bd0145f206a76b9ed1b
Nour-Mahgoub <s-nourhan.mahgoub@zewailcity.edu.eg>
2025-03-08 23:36:35 +0000

    execrcise4

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/exercise4/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/exercise4/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/exercise4/devel;/opt/ros/noetic' != '' else []
